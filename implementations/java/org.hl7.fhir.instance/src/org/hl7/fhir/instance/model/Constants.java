0f52328ef7089f5920c8c119b98ec03d1d003bf3
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-04-15 13:38:19 +0000

    git-svn-id: http://gforge.hl7.org/svn/fhir/dstu1/build@2364 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Constants.java
@@ -0,0 +1,8 @@
+package org.hl7.fhir.instance.model;
+
+public class Constants {
+
+  public final static String VERSION = "0.80";
+  public final static String REVISION = "2351";
+  public final static String DATE = "Mon Apr 14 08:04:18 EST 2014";
+}
