81d26f0e5a80a14979cbe6b1186cb41f2c6fd92f
ewoutkramer <ewoutkramer@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-07-15 15:32:39 +0000

    - [Implementations] updated copyright to 2014
    git-svn-id: http://gforge.hl7.org/svn/fhir/dstu1/build@2642 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/csharp/Hl7.Fhir.Model.Tests/Properties/AssemblyInfo.cs
+++ implementations/csharp/Hl7.Fhir.Model.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HL7 International")]
 [assembly: AssemblyProduct("FHIR")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.7.4")]
-[assembly: AssemblyFileVersion("0.0.7.4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
