93519a27f141e4f22f94f9a49b624e115716c14b
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-09-14 16:28:11 +0000

    add missing file
    git-svn-id: http://gforge.hl7.org/svn/fhir/dstu1/build@2851 2f0db536-2c49-4257-a3fa-e771ed206c19

---
+++ implementations/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/formats/ComposerBase.java
@@ -0,0 +1,78 @@
+package org.hl7.fhir.instance.formats;
+
+/*
+Copyright (c) 2011+, HL7, Inc
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this 
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, 
+   this list of conditions and the following disclaimer in the documentation 
+   and/or other materials provided with the distribution.
+ * Neither the name of HL7 nor the names of its contributors may be used to 
+   endorse or promote products derived from this software without specific 
+   prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import org.hl7.fhir.instance.model.AtomCategory;
+import org.hl7.fhir.instance.model.AtomFeed;
+import org.hl7.fhir.instance.model.Resource;
+
+public abstract class ComposerBase extends FormatUtilities implements Composer {
+
+  protected String xhtmlMessage;
+  
+  @Override
+  public void setSuppressXhtml(String message) {
+    xhtmlMessage = message;    
+  }
+
+  public String composeString(Resource resource, boolean pretty) throws Exception {
+    return new String(composeBytes(resource, pretty));
+  }
+
+  public String composeString(AtomFeed feed, boolean pretty) throws Exception {
+    return new String(composeBytes(feed, pretty));
+  }
+  
+  public String composeString(List<AtomCategory> tags, boolean pretty) throws Exception {
+    return new String(composeBytes(tags, pretty));
+  }
+  
+  public byte[] composeBytes(Resource resource, boolean pretty) throws Exception {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    compose(bytes, resource, true);
+    return bytes.toByteArray();
+  }
+
+  public byte[] composeBytes(AtomFeed feed, boolean pretty) throws Exception {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    compose(bytes, feed, true);
+    return bytes.toByteArray();
+  }
+  
+  public byte[] composeBytes(List<AtomCategory> tags, boolean pretty) throws Exception {
+    ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+    compose(bytes, tags, true);
+    return bytes.toByteArray();
+  }
+
+}
