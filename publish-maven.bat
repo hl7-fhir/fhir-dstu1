e6a28b51206a3cf36217e030d07c552aa603599e
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2014-09-30 07:05:30 +0000

    prep for release
    git-svn-id: http://gforge.hl7.org/svn/fhir/dstu1/build@2943 2f0db536-2c49-4257-a3fa-e771ed206c19

--- publish-maven.bat
+++ publish-maven.bat
@@ -1,6 +1,6 @@
-@echo Have you updated the version in this batch file? (0.0.82.2929)
+@echo Have you updated the version in this batch file? (0.0.82.2943)
 pause
-REM ant -f  tools/java/org.hl7.fhir.tools.core/build.xml  -DartifactId=fhir-dstu1 -Dversion=0.0.82.2929-SNAPSHOT deploy -e
+REM ant -f  tools/java/org.hl7.fhir.tools.core/build.xml  -DartifactId=fhir-dstu1 -Dversion=0.0.82.2943-SNAPSHOT deploy -e
 
-ant -f  tools/java/org.hl7.fhir.tools.core/build.xml  -DartifactId=fhir-dstu1 -Dversion=0.0.82.2929 stage -e
+ant -f  tools/java/org.hl7.fhir.tools.core/build.xml  -DartifactId=fhir-dstu1 -Dversion=0.0.82.2943 stage -e
 
